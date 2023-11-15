4433aadde21ed24df7a8a60a4f41fef907278d0b
HuynhAnhKhoi2052001 <anhkhoi2052001@gmail.com>
2023-11-15 17:02:49 +0700

    update homePage

--- YouTubeMusic/Screen1/homePage.js
+++ YouTubeMusic/Screen1/homePage.js
@@ -1,9 +1,27 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import React, { useState } from "react";
 import { LinearGradient } from "expo-linear-gradient";
-import { ScrollView } from "react-native-web";
-
+import FilterCard from "../conponents/FilterCard";
+import { FlatList } from "react-native-web";
 export default function HomeScreen() {
+  const [filter, setFilter] = useState([
+    "Nạp năng lượng",
+    "Thư giãn",
+    "Vui tươi",
+    "Tập thể dục",
+    "Tiệc tùng",
+    "Tập trung",
+    "Buồn",
+    "Lãng mạn",
+    "Dễ ngủ",
+  ]);
   return (
     <LinearGradient
       colors={["#72374E", "#603772", "#0E0E0E"]}
@@ -32,7 +50,14 @@ export default function HomeScreen() {
               ></Image>
             </View>
 
-            <View style={{ width:244,paddingRight:30,justifyContent: "center", alignItems: "flex-end" }}>
+            <View
+              style={{
+                width: 244,
+                paddingRight: 30,
+                justifyContent: "center",
+                alignItems: "flex-end",
+              }}
+            >
               <TouchableOpacity style={{ alignItems: "flex-end" }}>
                 <Image
                   source={require("../img/search.png")}
@@ -42,15 +67,25 @@ export default function HomeScreen() {
             </View>
 
             <View style={{ width: 54, paddingRight: 20 }}>
-              <TouchableOpacity style={{justifyContent:"center",alignItems:"center"}}>
+              <TouchableOpacity
+                style={{ justifyContent: "center", alignItems: "center" }}
+              >
                 <Image
                   source={require("../img/avt.png")}
                   style={{ width: 26, height: 26, resizeMode: "contain" }}
                 ></Image>
               </TouchableOpacity>
             </View>
-
           </View>
+          <ScrollView horizontal={true} showsVerticalScrollIndicator>
+            {filter.map((item, index) => (
+              <FilterCard item={item} key={index} />
+            ))}
+          </ScrollView>
+          <Text style ={{fontSize:24,fontStyle:'roboto',color:'white',fontWeight:'bold',marginLeft:16}}>Listen again</Text>
+          <ScrollView>
+            <FlatList></FlatList>
+          </ScrollView>
         </View>
       </ScrollView>
     </LinearGradient>
