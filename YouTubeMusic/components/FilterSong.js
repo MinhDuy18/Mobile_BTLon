50a3e696aecac4696313a33e3f9fcb821422ac98
HuynhAnhKhoi2052001 <anhkhoi2052001@gmail.com>
2023-11-25 17:24:57 +0700

    big update

---
+++ YouTubeMusic/components/FilterSong.js
@@ -0,0 +1,62 @@
+import { StyleSheet, Text, View,Image,TouchableOpacity,ScrollView,FlatList} from 'react-native'
+import React from 'react'
+
+const FilterSong = ({items}) => {
+  return (
+    <ScrollView
+    horizontal
+    showsHorizontalScrollIndicator={false}
+    directionalLockEnabled={true}
+    alwaysBounceVertical={false}
+  >
+    <FlatList
+      numColumns={3}
+      showsVerticalScrollIndicator={false}
+      showsHorizontalScrollIndicator={false}
+      data={items}
+      renderItem={({ item }) => {
+        return (
+          <TouchableOpacity
+            style={{
+              flexDirection: "row",
+              height: 60,
+              width: window.innerWidth,
+              justifyContent: "flex-start",
+              alignItems: "center",
+              paddingLeft: 20,
+            }}
+          >
+            <Image
+              source={item.image}
+              style={{
+                height: 50,
+                width: 50,
+                resizeMode: "contain",
+                borderRadius: 15,
+                marginRight: 10,
+              }}
+            ></Image>
+            <View
+              style={{
+                paddingHorizontal: 20,
+                justifyContent: "space-between",
+              }}
+            >
+              <Text style={{ fontSize: 16, color: "white" }}>
+                {item.name}
+              </Text>
+              <Text style={{ fontSize: 16, color: "grey" }}>
+                {item.singer}
+              </Text>
+            </View>
+          </TouchableOpacity>
+        );
+      }}
+    ></FlatList>
+  </ScrollView>
+  )
+}
+
+export default FilterSong
+
+const styles = StyleSheet.create({})
---
+++ YouTubeMusic/components/ListenAgain.js
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import React from "react";
+
+const ListenAgain = ({item }) => {
+  return (
+    <View>
+      <TouchableOpacity style={{width:120,justifyContent:"center",alignItems:"flex-start",marginHorizontal:5,marginVertical:10}}>
+        <Image
+          source={item.image}
+          style={{
+            width: 115,
+            height: 115,
+            resizeMode: "contain",
+            borderRadius: 10,
+          }}
+        ></Image>
+        <Text style={{fontSize:13,color:'white',fontStyle:'normal'}}>{item.name}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default ListenAgain;
+
+const styles = StyleSheet.create({});
